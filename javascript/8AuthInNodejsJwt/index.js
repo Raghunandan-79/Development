e82b4dae63945e8940fbb197a6707188e95b0740
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-14 15:52:37 +0530

    Added jwt logic

--- javascript/8AuthInNodejsJwt/index.js
+++ javascript/8AuthInNodejsJwt/index.js
@@ -1,38 +1,17 @@
+require("dotenv").config()
+
 const express = require("express")
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = process.env.JWT_SECRET
 const app = express()
 
 app.use(express.json())
 
 const users = []
 
-// should return a random long string
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', 
-        '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-
-    return token;
-}
-
 app.post("/signup", function(req, res) {
     const username = req.body.username
     const password = req.body.password
-
-    // if (users.find(function(user) {
-    //     if (user.username === username) {
-    //         return res.json({
-    //             message: "User with username exists"
-    //         })
-    //     }
-    // }))
 
     if (users.find(u => u.username === username)) {
         return res.json({
@@ -55,27 +34,21 @@ app.post("/signin", function(req, res) {
     const username = req.body.username
     const password = req.body.password
 
-    // const user = users.find(function(u) {
-    //     if (u.username === username && u.password === password) {
-    //         return true
-    //     } else {
-    //         return false
-    //     }
-    // })
-
     const user = users.find(
         user => user.username === username && user.password === password
     )
 
     if (user) {
-        const token = generateToken()
-        user.token = token
-
-        res.json({
-            message: token
-        })
+        // Create a token
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET)
 
         console.log(users)
+        
+        res.json({
+            token: token
+        })
     } else {
         return res.status(403).send({
             message: "Invalid username or password" 
@@ -84,10 +57,12 @@ app.post("/signin", function(req, res) {
 })
 
 app.get("/me", (req, res) => {
-    const token = req.headers.token
+    const token = req.headers.token  // jwt
+    const decodedInformation = jwt.verify(token, JWT_SECRET) // json object
+    const username = decodedInformation.username
 
     const user = users.find(
-        user => user.token === token
+        user => user.username === username
     )
 
     if (user) {
