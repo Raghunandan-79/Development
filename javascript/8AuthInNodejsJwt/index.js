f138deb95d10564d7b6e67447dab2a6bd131658b
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-14 15:18:58 +0530

    Basic authentication app

---
+++ javascript/8AuthInNodejsJwt/index.js
@@ -0,0 +1,107 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const users = []
+
+// should return a random long string
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', 
+        '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+
+    return token;
+}
+
+app.post("/signup", function(req, res) {
+    const username = req.body.username
+    const password = req.body.password
+
+    // if (users.find(function(user) {
+    //     if (user.username === username) {
+    //         return res.json({
+    //             message: "User with username exists"
+    //         })
+    //     }
+    // }))
+
+    if (users.find(u => u.username === username)) {
+        return res.json({
+            message: "User with username exits"
+        })
+    }
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    console.log(users)
+    res.json({
+        message: "Signup complete"
+    })
+})
+
+app.post("/signin", function(req, res) {
+    const username = req.body.username
+    const password = req.body.password
+
+    // const user = users.find(function(u) {
+    //     if (u.username === username && u.password === password) {
+    //         return true
+    //     } else {
+    //         return false
+    //     }
+    // })
+
+    const user = users.find(
+        user => user.username === username && user.password === password
+    )
+
+    if (user) {
+        const token = generateToken()
+        user.token = token
+
+        res.json({
+            message: token
+        })
+
+        console.log(users)
+    } else {
+        return res.status(403).send({
+            message: "Invalid username or password" 
+        })
+    }
+})
+
+app.get("/me", (req, res) => {
+    const token = req.headers.token
+
+    const user = users.find(
+        user => user.token === token
+    )
+
+    if (user) {
+        res.json({
+            username: user.username,
+            password: user.password
+        })
+    } else {
+        return res.status(400).send({
+            message: "Can't find the user"
+        })
+    }
+})
+
+app.listen(3000, () => {
+    console.log(`Server running on port 3000`)
+})
