a695a0fa58ff882194e9637588aa71dd6bd45952
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-15 19:29:29 +0530

    Added zod and salting

--- javascript/9MongoDbZodErrorHandlingAndInputValidation/index.js
+++ javascript/9MongoDbZodErrorHandlingAndInputValidation/index.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
 const express = require("express")
-const { UserModel, TodoModel } = require("./db")
 const mongoose = require("mongoose")
 const jwt = require("jsonwebtoken")
-const { todo } = require("node:test")
+const bcrypt = require("bcrypt")
+const { UserModel, TodoModel } = require("./db")
 const { auth, JWT_SECRET } = require("./auth")
+const { z } = require("zod")
 const app = express()
 
 mongoose.connect(process.env.MONGO_CONNECT)
@@ -12,19 +12,56 @@ mongoose.connect(process.env.MONGO_CONNECT)
 app.use(express.json())
 
 app.post("/signup", async (req, res) => {
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        name: z.string().min(3).max(100),
+        password: z.string().min(3).max(30).regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+            {
+                message:
+                    "Password must contain 1 uppercase, 1 lowercase, 1 number and 1 special character"
+            }
+        )
+    }).strict()
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body)
+
+    if (!parsedDataWithSuccess.success) {
+        return res.json({
+            message: "Incorrect form",
+            error: parsedDataWithSuccess.error
+        })
+    }
+
     const email = req.body.email
     const password = req.body.password
     const name = req.body.name
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    })
+    let errorThrown = false
 
-    res.json({
-        message: "You are logged in"
-    })
+    try {
+        const hashedPassword = await bcrypt.hash(password, 5)
+
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        })
+
+        
+    } catch (e) {
+        res._construct(400).json({
+            message: "User with email already exists"
+        })
+
+        errorThrown === true
+    }
+
+    if (!errorThrown) {
+        res.json({
+            message: "You are signed in"
+        })
+    }
 })
 
 app.post("/signin", async (req, res) => {
@@ -32,13 +69,20 @@ app.post("/signin", async (req, res) => {
     const password = req.body.password
 
     const user = await UserModel.findOne({
-        email: email,
-        password: password
+        email: email
     })
+
+    if (!user) {
+        return res.status(403).json({
+            message: "User doesnot exists"
+        })
+    }
+
+    const passwordMatched = await bcrypt.compare(password, user.password)
 
     console.log(user)
 
-    if (user) {
+    if (passwordMatched) {
         const token = jwt.sign({
             id: user._id.toString()
         }, JWT_SECRET)
