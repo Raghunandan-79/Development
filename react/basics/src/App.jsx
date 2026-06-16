62e49cb69858896a53bd20ba92eb9e2bb097041e
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-16 16:55:23 +0530

    Added basic todo logic

--- react/basics/src/App.jsx
+++ react/basics/src/App.jsx
@@ -1,15 +1,42 @@
 import React, { useState } from 'react'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      title: "Go to gym",
+      description: "Hit the gym regularly",
+      done: false
+    },
+  ])
 
-  function onClickHandler() {
-    setCount(count + 1)
+  function addTodo() {
+    let newArray = [...todos]
+    
+    newArray.push({
+      title: document.getElementById("title").value,
+      description: document.getElementById("description").value,
+      done: true
+    })
+
+    setTodos(newArray)
   }
 
-  return (
-    <div>
-      <button id='btn' onClick={onClickHandler}>Counter {count}</button>
-    </div>
-  )
+  return <div>
+    <input id='title' type="text" placeholder='Title'/>
+    <input id='description' type="text" placeholder="Description"/>
+    <button onClick={addTodo}>Add TODO</button>
+    {todos.map((todo) => <Todo 
+      title={todo.title} 
+      description={todo.description} 
+      done={todo.done}
+    />)}
+  </div>
+}
+
+function Todo(props) {
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+    <h1>{props.done ? "Task is done" : "Task is not done"}</h1>
+  </div>
 }
