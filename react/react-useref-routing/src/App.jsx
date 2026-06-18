9f4d72f97094ce08252e6560336c46018ebfa3c2
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-18 07:05:41 +0530

    Clock with userref

--- react/react-useref-routing/src/App.jsx
+++ react/react-useref-routing/src/App.jsx
@@ -86,12 +86,31 @@
     triggers a re-render when updated
 */
 
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
+// clock with start and stop button
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [currentCount, setCurrentCount] = useState(1)
+  const timer = useRef()
+
+  function startClock() {
+    let value = setInterval(() => {
+      setCurrentCount(c => c + 1)
+    }, 1000)
+    timer.current = value
+  }
+
+  function stopClock() {
+    console.log(timer)
+    clearInterval(timer.current)
+  }
+
+  return <div>
+    {currentCount}
+    <br />
+    <button onClick={startClock}>Start</button>
+    <button onClick={stopClock}>Stop</button>
+  </div>
 }
 
 export default App
