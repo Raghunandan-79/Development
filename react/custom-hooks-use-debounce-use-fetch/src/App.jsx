464a380d0148923426790f68f050e54e59a92186
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-19 23:46:54 +0530

    Custom hooks, use debounce and use fetch

--- react/custom-hooks-use-debounce-use-fetch/src/App.jsx
+++ react/custom-hooks-use-debounce-use-fetch/src/App.jsx
@@ -1,120 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function useCounter() {
+//   const [count, setCount] = useState(0)
 
-  return (
+//   function increaseCount() {
+//     setCount(count + 1)
+//     // setCount(c => c + 1)
+//   }
+
+//   return {
+//     count: count,
+//     increaseCount: increaseCount
+//   }
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Counter />
+//       <Counter />
+//       <Counter />
+//       <Counter />
+//       <Counter />
+//     </div>
+//   )
+// }
+
+// function Counter() {
+//   const { count, increaseCount } = useCounter()
+
+//   return <div>
+//     <button onClick={increaseCount}>Increase {count}</button>
+//   </div>
+// }
+
+// export default App
+
+
+// import React, { useEffect, useState } from 'react'
+// import { useFetch } from './hooks/useFetch'
+
+
+
+// const App = () => {
+//   const [currentPost, setCurrentPost] = useState(1)
+//   const { finalData, loading } = useFetch(`https://jsonplaceholder.typicode.com/posts/${currentPost}`, 5)
+
+//   if (loading) {
+//     return <div>
+//       Loading....
+//     </div>
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={() => setCurrentPost(1)}>1</button>
+//       <button onClick={() => setCurrentPost(2)}>2</button>
+//       <button onClick={() => setCurrentPost(3)}>3</button>
+//       {JSON.stringify(finalData)}
+//     </div>
+//   )
+// }
+
+// export default App  
+
+
+// import React, { useState } from 'react'
+// import { usePrev } from './hooks/usePrev'
+
+// const App = () => {
+//   const [state, setState] = useState(0)
+//   const prev = usePrev(state)
+
+//   return (
+//     <div>
+//       <p>{state}</p>
+//       <button onClick={() => {
+//         setState(curr => curr + 1)
+//       }}
+//       >Click Me</button>
+//       <p>The previos value was {prev}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useEffect, useRef, useState } from 'react'
+
+
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
+
+const App = () => {
+  const [inputVal, setInputVal] = useState("")
+  const debouncedValue = useDebounce(inputVal, 200)
+
+  function change(e) {
+    setInputVal(e.target.value)
+  }
+
+  useEffect(() => {
+    // expensive operation
+    // fetch
+    console.log("expensive operation")
+  }, [debouncedValue])
+
+  return (  
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <input id="input" type="text" onChange={change} />
     </>
   )
 }
