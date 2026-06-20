18149181bfaf3bcd09e44f7d743b4295798bd13f
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-21 01:31:15 +0530

    Added logic to fetch from apis

--- react/redux-project/src/App.jsx
+++ react/redux-project/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { fetchPhotos, fetchVideos } from './api/mediaApi'
 
 const App = () => {
   return (
     <div className='h-screen w-full bg-gray-950 text-white'>
-      
+      <button className='bg-green-400 px-4 py-5 m-5'   
+        onClick={async () => {
+          const data = await fetchPhotos('cat')
+      }}>Get photos</button>
+
+      <button className='bg-green-400 px-4 py-5 m-5'  
+        onClick={async () => {
+          const data = await fetchVideos('cat')
+      }}>
+        Get videos
+      </button>
     </div>
   )
 }
---
+++ react/redux-project/src/api/mediaApi.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
+
+export async function fetchPhotos(query, page=1, perPage=20) {
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
+        params: {
+            query,
+            page,
+            perPage
+        },
+
+        headers: {
+            Authorization: `Client-ID ${UNSPLASH_KEY}`
+        }
+    })
+
+    return res.data
+}
+
+export async function fetchVideos(query, perPage=15) {
+    const res = await axios.get("https://api.pexels.com/videos/search", {
+        params: {
+            query,
+            perPage
+        },
+
+        headers: {
+            Authorization: PEXELS_KEY
+        }
+    })
+
+    return res.data
+}
