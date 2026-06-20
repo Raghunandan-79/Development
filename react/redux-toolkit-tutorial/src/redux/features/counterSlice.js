e011b7b41bc7a09b10bac8d875dd907705c5b636
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-21 00:20:53 +0530

    Redux toolkit tutorial

---
+++ react/redux-toolkit-tutorial/src/redux/features/counterSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+    name: "counter",
+    
+    initialState: {
+        value: 0
+    },
+
+    reducers: {
+        increment: (state) => {
+            state.value += 1
+        },
+
+        decrement: (state) => {
+            if (state.value == 0) {
+                alert("Cannot decrease count below 0")
+                return
+            }
+
+            state.value -= 1
+        },
+
+        increaseByAmount: (state, actions) => {
+            state.value += actions.payload
+        }
+    }
+})
+
+export const {
+    increment,
+    decrement,
+    increaseByAmount
+} = counterSlice.actions
+
+export default counterSlice.reducer
