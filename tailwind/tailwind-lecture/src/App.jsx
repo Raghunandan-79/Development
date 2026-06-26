bcd5759016497a1d7c963eec8d42936e701704e1
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-26 18:53:05 +0530

    Learning responsiveness

--- tailwind/tailwind-lecture/src/App.jsx
+++ tailwind/tailwind-lecture/src/App.jsx
@@ -4,15 +4,15 @@ const App = () => {
   return (
     <>
       <div className='grid grid-row-3 sm:grid-cols-12'>
-        <div className='row-span-1 sm:col-span-4 bg-red-300'>
+        <div className='col-span-12 sm:col-span-4 bg-red-300'>
           Hi
         </div>
 
-        <div className='row-span-1 sm:col-span-4 bg-green-300'>
+        <div className='col-span-12 sm:col-span-4 bg-green-300'>
           Hi
         </div>
 
-        <div className='row-span-1 sm:col-span-4 bg-blue-300'>
+        <div className='col-span-12 sm:col-span-4 bg-blue-300'>
           Hi
         </div>
       </div>
