98dca91594743c975ff36c314f93344f406d64dc
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-04 20:40:47 +0530

    Learned joins

--- postgres-sql/postgres-and-sql-databases-part-2/src/index.ts
+++ postgres-sql/postgres-and-sql-databases-part-2/src/index.ts
@@ -51,6 +51,40 @@ app.post("/signup", async (req, res) => {
     });
 });
 
+app.get("/metadata", async (req, res) => {
+    const id = req.query.id;
+
+    const query1 = `SELECT username, email, id FROM users WHERE id=$1`;
+    const query2 = `SELECT * FROM addresses WHERE user_id=$1`;
+
+    await pgClient.query(`BEGIN;`);
+    const response1 = await pgClient.query(query1, [id]);
+    const response2 = await pgClient.query(query2, [id]);
+    await pgClient.query(`COMMIT`)
+
+    res.json({
+        user: response1.rows[0],
+        address: response2.rows
+    });
+});
+
+app.get("/better-metadata", async (req, res) => {
+    const id = req.query.id;
+    // const query = `SELECT users.id, users.username, users.email, addresses.city, addresses.country, addresses.street, addresses.pincode FROM users
+    // JOIN addresses ON users.id = addresses.user_id
+    // WHERE users.id = $1;`;
+
+    const query = `SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode FROM users u
+    JOIN addresses a ON u.id = a.user_id
+    WHERE u.id = $1`
+
+    const response = await pgClient.query(query, [id]);
+    
+    res.json({
+        data: response.rows
+    })
+});
+
 async function main() {
     try {
         await pgClient.connect();
