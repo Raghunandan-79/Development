b34370dd12b989ab5704503143d8ef6b5a9ef115
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-05 20:44:31 +0530

    Added backend endpoints

---
+++ nextjs/nextjs-continuation/app/api/v1/user/details/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export function GET() {
+  return NextResponse.json({
+    user: "harkirat",
+    email: "harkirat@gmail.com",
+  });
+}
+
+export function POST() {
+  return NextResponse.json({
+    user: "harkirat",
+    email: "harkirat@gmail.com",
+  });
+}
+
+export function PUT() {
+  return NextResponse.json({
+    user: "harkirat",
+    email: "harkirat@gmail.com",
+  });
+}
---
+++ nextjs/nextjs-continuation/app/auth/signin/page.tsx
@@ -0,0 +1,11 @@
+export default function Signin() {
+    return <div>
+        Signin page
+
+        <input type="text" placeholder="email" />
+
+        <input type="password" placeholder="password" />
+
+        <button>Sign in</button>
+    </div>
+}
