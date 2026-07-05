fbd67b0aecf0a69d23c99f879c437669faf9b65a
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-05 14:48:42 +0530

    Initialized a new server

---
+++ prisma-and-orm/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Healthy",
+  });
+});
+
+async function main() {
+  try {
+    app.listen(3000, () => {
+      console.log("Server is up and running on port 3000");
+    });
+  } catch (e) {
+    console.log("Unable to start the server");
+  }
+}
+
+main();
