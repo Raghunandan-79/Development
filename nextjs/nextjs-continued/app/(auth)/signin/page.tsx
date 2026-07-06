b5b1f3b0ceb58368e984dfc7038e59385e6afb73
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-06 18:34:25 +0530

    Learned route groups and layout

---
+++ nextjs/nextjs-continued/app/(auth)/signin/page.tsx
@@ -0,0 +1,10 @@
+export default function Signin() {
+  return (
+    <div className="flex flex-col">
+      Signin
+      <input type="text" />
+      <input type="text" />
+      <button>Sign in</button>
+    </div>
+  );
+}
---
+++ nextjs/nextjs-continued/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function Signup() {
+  return (
+    <div>
+      Signup
+      <input type="text" />
+      <input type="text" />
+      <button>Sign up</button>
+    </div>
+  );
+}
